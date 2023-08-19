029f35e8e174345157d426cc6cd2c69fb548ab4c
maple <github.register@slmail.me>
2023-08-19 20:49:37 +0200

    🐛 fix: header

--- header/cub3d.h
+++ header/cub3d.h
@@ -6,17 +6,27 @@
 /*   By: croy <croy@student.42lyon.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/16 15:01:34 by croy              #+#    #+#             */
-/*   Updated: 2023/08/16 15:11:41 by croy             ###   ########lyon.fr   */
+/*   Updated: 2023/08/19 20:49:14 by croy             ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CUB3D_H
 # define CUB3D_H
 
-# include "../mlx/mlx.h"
+# include "../minilibx/mlx.h"
 # include "libft.h"
 
+# include <errno.h>  // for perror
+# include <fcntl.h>  // for open
+# include <stdio.h>  // for printf
+# include <stdlib.h> // for malloc, free, exit
+# include <string.h> // for strerror
+# include <unistd.h> // for close, read, write
+
 //	===== @functions =====
+// parsing.c
+void	print_aha(void);
+
 // cub3d.c
 int	print_lol(void);
 
