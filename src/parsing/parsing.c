f6a08a1296865225b55eb3087ddeb8661da788dd
maple <28269333+maplepy@users.noreply.github.com>
2023-09-18 13:00:03 +0000

    📚 docs: add doc

--- src/parsing/parsing.c
+++ src/parsing/parsing.c
@@ -6,11 +6,20 @@
 /*   By: croy <croy@student.42lyon.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/19 19:55:57 by croy              #+#    #+#             */
-/*   Updated: 2023/08/19 21:40:06 by croy             ###   ########lyon.fr   */
+/*   Updated: 2023/09/18 12:59:30 by croy             ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
+
+/**
+ * @brief Check if the file has the right extension
+ *
+ * @param data	pointer to the main structure
+ * @param file	absolute or relative path to the file
+ * @param ext	extension to check
+ * @return int	EXIT_SUCCESS or EXIT_FAILURE
+ */
 static int	check_extension(char *path, char *ext)
 {
 	int		file_len;
@@ -35,6 +44,13 @@ static int	check_extension(char *path, char *ext)
 	return (EXIT_SUCCESS);
 }
 
+/**
+ * @brief Parse and check the map file
+ *
+ * @param data	pointer to the main structure
+ * @param map	absolute or relative path to the map
+ * @return int	EXIT_SUCCESS or EXIT_FAILURE
+ */
 int	map_parsing(t_data *data, char *map)
 {
 	(void) data;
