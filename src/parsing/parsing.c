ca34de9fad805a4f287a0543b6521516c5687da7
maple <github.register@slmail.me>
2023-09-22 17:17:48 +0200

    🔍 debug: a \n to be clearer

--- src/parsing/parsing.c
+++ src/parsing/parsing.c
@@ -6,7 +6,7 @@
 /*   By: croy <croy@student.42lyon.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/19 19:55:57 by croy              #+#    #+#             */
-/*   Updated: 2023/09/22 17:06:49 by croy             ###   ########lyon.fr   */
+/*   Updated: 2023/09/22 17:12:57 by croy             ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -154,7 +154,7 @@ static int	set_color(unsigned *color, char *input)
 		i++;
 	}
 	free_tab(colors);
-	printf("\e[92;1mRGB: \e[0m%d, %d, %d\n", rgb[0], rgb[1], rgb[2]); // REMOVE
+	printf("\n\e[92;1mRGB: \e[0m%d, %d, %d\n", rgb[0], rgb[1], rgb[2]); // REMOVE
 	*color = rgb[0] << 16 | rgb[1] << 8 | rgb[2]; // THIS NEEDS TO BE CONVERTED TO REAL HEX
 	printf("\e[92;1mHEX: \e[0m%d\n", *color); // REMOVE
 	// print_nbr(*color, "0123456789abcdef"); // REMOVE
@@ -219,12 +219,12 @@ static int	get_map_properties(t_data *data)
 		free_tab(lines);
 		i++;
 	}
-	printf("\e[92;1mTextures:\e[0m\n"); // REMOVE
+	printf("\n\e[92;1mTextures:\e[0m\n"); // REMOVE
 	printf("\e[93;1mnorth: \e[22m%s\e[0m\n", data->north.path); // REMOVE
 	printf("\e[93;1msouth: \e[22m%s\e[0m\n", data->south.path); // REMOVE
 	printf("\e[93;1mwest: \e[22m%s\e[0m\n", data->west.path); // REMOVE
 	printf("\e[93;1meast: \e[22m%s\e[0m\n", data->east.path); // REMOVE
-	printf("\e[92;1mColors:\e[0m\n"); // REMOVE
+	printf("\n\e[92;1mColors:\e[0m\n"); // REMOVE
 	printf("\e[93;1mfloor: \e[22m%d\e[0m\n", data->floor); // REMOVE
 	printf("\e[93;1mceiling: \e[22m%d\e[0m\n", data->ceiling); // REMOVE
 	return (EXIT_SUCCESS);
