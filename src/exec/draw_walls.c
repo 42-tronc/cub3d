6aa644d55dd4827205c217e4d09d77b5057fae9b
boulatrousse <lboulatrous@gmail.com>
2023-10-26 15:14:05 +0200

    adding draw_floor function

--- src/exec/draw_walls.c
+++ src/exec/draw_walls.c
@@ -6,13 +6,14 @@
 /*   By: lboulatr <lboulatr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/18 10:16:24 by lboulatr          #+#    #+#             */
-/*   Updated: 2023/10/26 14:00:34 by lboulatr         ###   ########.fr       */
+/*   Updated: 2023/10/26 15:00:44 by lboulatr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
 static void	get_textures(t_data *data, t_ray *ray, int y, int x, int tex_y);
+static void	draw_floor(t_data *data, int width, int i);
 
 void	draw_walls(t_data *data, t_ray *ray, int width)
 {
@@ -27,20 +28,16 @@ void	draw_walls(t_data *data, t_ray *ray, int width)
 	wall_height = WALL_H / ray->length;
 	while (i < (HEIGHT / 2) - (wall_height / 2))
 	{
-		put_pixel(&data->minimap, abs(width), i, data->ceiling);
+		put_pixel(&data->minimap, width, i, data->ceiling);
 		i++;
 	}
 	while (i < (HEIGHT / 2) + (wall_height / 2))
 	{
-		get_textures(data, ray, abs(width), i, tex_y);
+		get_textures(data, ray, width, i, tex_y);
 		tex_y++;
 		i++;
 	}
-	while (i < HEIGHT)
-	{
-		put_pixel(&data->minimap, abs(width), i, data->floor);
-		i++;
-	}
+	draw_floor(data, width, i);
 }
 
 static void	get_textures(t_data *data, t_ray *ray, int y, int x, int tex_y)
@@ -51,21 +48,30 @@ static void	get_textures(t_data *data, t_ray *ray, int y, int x, int tex_y)
 	if (ray->cardinal == NORTH)
 	{
 		dst = draw_textures(data, ray, tex_y);
-		put_pixel(&data->minimap, abs(y), x, *(unsigned int *)dst);
+		put_pixel(&data->minimap, y, x, *(unsigned int *)dst);
 	}
 	if (ray->cardinal == SOUTH)
 	{
 		dst = draw_textures(data, ray, tex_y);
-		put_pixel(&data->minimap, abs(y), x, *(unsigned int *)dst);
+		put_pixel(&data->minimap, y, x, *(unsigned int *)dst);
 	}
 	if (ray->cardinal == WEST)
 	{
 		dst = draw_textures(data, ray, tex_y);
-		put_pixel(&data->minimap, abs(y), x, *(unsigned int *)dst);
+		put_pixel(&data->minimap, y, x, *(unsigned int *)dst);
 	}
 	if (ray->cardinal == EAST)
 	{
 		dst = draw_textures(data, ray, tex_y);
-		put_pixel(&data->minimap, abs(y), x, *(unsigned int *)dst);
+		put_pixel(&data->minimap, y, x, *(unsigned int *)dst);
+	}
+}
+
+static void	draw_floor(t_data *data, int width, int i)
+{
+	while (i < HEIGHT)
+	{
+		put_pixel(&data->minimap, width, i, data->floor);
+		i++;
 	}
 }
