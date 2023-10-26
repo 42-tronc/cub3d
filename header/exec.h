56c024a0703ae777f1d5cb7c412696f455921fdd
maple <github.register@slmail.me>
2023-10-26 15:24:38 +0200

    🔄 update: norm exec.h

--- header/exec.h
+++ header/exec.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   exec.h                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: lboulatr <lboulatr@student.42.fr>          +#+  +:+       +#+        */
+/*   By: croy <croy@student.42lyon.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/22 07:34:12 by lboulatr          #+#    #+#             */
-/*   Updated: 2023/10/26 14:40:54 by lboulatr         ###   ########.fr       */
+/*   Updated: 2023/10/26 15:24:17 by croy             ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,29 +19,29 @@
 # include "cub3d.h"
 # include "struct.h"
 
-# define SUCCESS	    EXIT_SUCCESS
-# define FAILURE	    EXIT_FAILURE
-# define IMG_ERR        1
+# define SUCCESS		EXIT_SUCCESS
+# define FAILURE		EXIT_FAILURE
+# define IMG_ERR		1
 
 // ===== Define Keys =====
-# define KEY_ESC 	    65307
-# define LEFT_ARROW     65361
-# define RIGHT_ARROW    65363
-# define KEY_W 	        119
-# define KEY_A	        97
-# define KEY_S	        115
-# define KEY_D	        100
+# define KEY_ESC		65307
+# define LEFT_ARROW		65361
+# define RIGHT_ARROW	65363
+# define KEY_W			119
+# define KEY_A			97
+# define KEY_S			115
+# define KEY_D			100
 
 // ===== Game define =====
-# define BLOCK          20
-# define SIZE_MAP       20
+# define BLOCK			20
+# define SIZE_MAP		20
 # define SIZE_WALL		125
 # define SIZE_CUT		4
 # define EYE_SIGHT		540
 # define TOL			0.025
 # define MIN_LEN_RAY	0.0886
-# define HEIGHT         1080
-# define WIDTH          1920
+# define HEIGHT			1080
+# define WIDTH			1920
 # define WALL_H			1500
 
 // ===== Maths define =====
@@ -52,11 +52,11 @@
 # define MAP_WALL		0x0f056b
 # define MAP_SPACE		0x919191
 # define MAP_PLAYER		0x00ff7f
-# define GREEN_HEX      0x7CFC00
-# define BLUE_HEX       0x87CEEB
-# define YELLOW_HEX     0xffff00
+# define GREEN_HEX		0x7CFC00
+# define BLUE_HEX		0x87CEEB
+# define YELLOW_HEX		0xffff00
 # define GREY_HEX		0x7f7f7f
-# define N_RED	        0xff0000
+# define N_RED			0xff0000
 # define S_BROWN		0xb07922
 # define E_GREEN		0x2d9c18
 # define W_WHITE		0xf0ede5
@@ -77,7 +77,7 @@ void			init_ray_struct(t_data *data, t_ray *ray, \
 void			draw_minimap(t_data *data);
 void			draw_player_minimap(t_mlx_data *minimap, float x, float y);
 void			draw_walls(t_data *data, t_ray *ray, int width);
-char	*draw_textures(t_data *data, t_ray *ray, int y);
+char			*draw_textures(t_data *data, t_ray *ray, int y);
 int				refresh_window(t_data *data);
 
 // ========== Moves ==========
